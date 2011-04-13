96f0eed3f1b2a1eefc6a86aa3ba528ebc79c1395
Arran Walker <github@fiveturns.org>
2011-04-13 13:10:42 +0100

    Improved doctrine 2 integration

--- app/lib/database/Doctrine2Database.class.php
+++ app/lib/database/Doctrine2Database.class.php
@@ -11,60 +11,64 @@ class Doctrine2Database extends AgaviDatabase
 
 	public function shutdown()
 	{
-		if ($this->em->getUnitOfWork()->size()) {
-			$this->em->flush();
-		}
 	}
 
 	public function getEntityManager()
 	{
+		if ($this->em === null) {
+			$parameters = $this->getParameters();
+
+			$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\ORM', $parameters['orm_path']);
+			$classLoader->register();
+
+			$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', $parameters['dbal_path']);
+			$classLoader->register();
+
+			$classLoader = new \Doctrine\Common\ClassLoader($parameters['proxy_namespace'], $parameters['proxy_path']);
+			$classLoader->register();
+
+			$config = new Doctrine2Configuration();
+			$config->initialize($this->databaseManager->getContext());
+
+			if (!AgaviConfig::get('core.debug')) {
+				$config->setMetadataCacheImpl(new Doctrine\Common\Cache\ApcCache());
+				$config->setQueryCacheImpl(new Doctrine\Common\Cache\ApcCache());
+				$config->setResultCacheImpl(new \Doctrine\Common\Cache\ApcCache());
+			}
+
+			$driverImpl = $config->newDefaultAnnotationDriver(array($parameters['entities_path']));
+			$config->setMetadataDriverImpl($driverImpl);
+	
+			$config->setProxyDir($parameters['proxy_path'].'/'.str_replace('\\', '_', $parameters['proxy_namespace']));
+			$config->setProxyNamespace($parameters['proxy_namespace']);
+
+			$connectionParams = array(
+				'dbname'   => $parameters['database'],
+				'user'     => $parameters['username'],
+				'password' => $parameters['password'],
+				'host'     => $parameters['hostname'],
+				'driver'   => $parameters['driver']
+			);
+
+			$evm = new \Doctrine\Common\EventManager();
+			$evm->addEventSubscriber(new \Doctrine\DBAL\Event\Listeners\MysqlSessionInit($this->getParameter('charset')));
+			$evm->addEventListener(array(Doctrine\ORM\Events::postLoad), new Doctrine2ContextEventListener());
+
+			$this->em = \Doctrine\ORM\EntityManager::create($connectionParams, $config, $evm);
+		}
+		
 		return $this->em;
 	}
 
 	public function initialize(AgaviDatabaseManager $databaseManager, array $parameters = array())
 	{
 		parent::initialize($databaseManager, $parameters);
-
-		// Doctrine autoload
-		$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\ORM', $parameters['orm_path']);
-		$classLoader->register();
-
-		$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', $parameters['dbal_path']);
-		$classLoader->register();
-
+		
 		$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', $parameters['common_path']);
 		$classLoader->register();
 
-		$classLoader = new \Doctrine\Common\ClassLoader($parameters['proxy_namespace'], $parameters['proxy_path']);
+		$classLoader = new \Doctrine\Common\ClassLoader($parameters['entities_namespace'], $parameters['entities_path']);
 		$classLoader->register();
-
-		$config = new Doctrine2Configuration();
-		$config->initialize($databaseManager->getContext());
-
-		if (!AgaviConfig::get('core.debug')) {
-			$config->setMetadataCacheImpl(new Doctrine\Common\Cache\ApcCache());
-			$config->setQueryCacheImpl(new Doctrine\Common\Cache\ApcCache());
-		}
-
-		$driverImpl = $config->newDefaultAnnotationDriver(array($parameters['entities']));
-		$config->setMetadataDriverImpl($driverImpl);
-
-		$config->setProxyDir($parameters['proxy_path'].'/'.str_replace('\\', '_', $parameters['proxy_namespace']));
-		$config->setProxyNamespace($parameters['proxy_namespace']);
-
-		$connectionParams = array(
-			'dbname'   => $parameters['database'],
-			'user'     => $parameters['username'],
-			'password' => $parameters['password'],
-			'host'     => $parameters['hostname'],
-			'driver'   => $parameters['driver']
-		);
-
-		$evm = new \Doctrine\Common\EventManager();
-		$evm->addEventSubscriber(new \Doctrine\DBAL\Event\Listeners\MysqlSessionInit($this->getParameter('charset')));
-		$evm->addEventListener(array(Doctrine\ORM\Events::postLoad), new Doctrine2ContextEventListener());
-
-		$this->em = \Doctrine\ORM\EntityManager::create($connectionParams, $config, $evm);
 	}
 }
 
